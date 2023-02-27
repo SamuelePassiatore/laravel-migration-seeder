e1b81fb8edf35b4bd67fc954faa2de469869dd88
SamuelePassiatore <semmisno2@protonmail.com>
2023-02-27 17:15:15 +0100

    print on page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,4 +4,22 @@
 
 @section('content')
     Tabella treni
+    <ul>
+        @foreach ($trains as $train)
+            <li>
+                <ul class="mb-3">
+                    <li><strong>Date: </strong>{{ $train->date }}</li>
+                    <li><strong>Company: </strong>{{ $train->company }}</li>
+                    <li><strong>Departure Station: </strong>{{ $train->departure_station }}</li>
+                    <li><strong>Arrival Station: </strong>{{ $train->arrival_station }}</li>
+                    <li><strong>Departure Time: </strong>{{ $train->departure_time }}</li>
+                    <li><strong>Arrival Time: </strong>{{ $train->arrival_time }}</li>
+                    <li><strong>Train Code: </strong>{{ $train->train_code }}</li>
+                    <li><strong>Carriages Number: </strong>{{ $train->carriages_number }}</li>
+                    <li><strong>On time: </strong>{{ $train->on_time }}</li>
+                    <li><strong>Deleted: </strong>{{ $train->deleted }}</li>
+                </ul>
+            </li>
+        @endforeach
+    </ul>
 @endsection
