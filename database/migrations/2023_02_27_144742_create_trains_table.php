beeac56bb3567d93e3593cc5cc9d59c9c259d500
SamuelePassiatore <semmisno2@protonmail.com>
2023-02-27 16:16:22 +0100

    created migration and table trains

---
+++ database/migrations/2023_02_27_144742_create_trains_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 40);
+            $table->string('departure_station', 50);
+            $table->string('arrival_station', 50);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('train_code', 15);
+            $table->tinyInteger('carriages_number')->unsigned();
+            $table->boolean('on_time');
+            $table->boolean('deleted')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
